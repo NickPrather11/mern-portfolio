b9f967470f42e607c6a049d5b9da56bfb89bc2e0
Flying Serpent <nickprather11@gmail.com>
2019-10-30 16:54:38 -0400

    built project objects as an array inside seedBuilder.js in controllers directory

---
+++ controllers/seedBuilder.js
@@ -0,0 +1,68 @@
+import Project from "../models/projectModel";
+
+const projectsArray = [
+  {
+    name: "Gluttony Game",
+    image: "/client/public/images/Gluttony_Game.PNG",
+    deployedLink: "https://nickprather11.github.io/unit-4-game/",
+    githubLink: "https://github.com/NickPrather11/unit-4-game",
+    description:
+      "The goal of this game is for the user to reach the exact 'goal' number by clicking on the various images, which have associated number values between 1 and 12. However, each image's number value is randomly generated each round."
+  },
+  {
+    name: "Lebowski Trivia",
+    image: "/client/public/images/Lebowski_Trivia.PNG",
+    deployedLink: "https://nickprather11.github.io/TriviaGame/",
+    githubLink: "",
+    description: ""
+  },
+  {
+    name: "LIRI Node App",
+    image: "/client/public/images/LIRI-movie-this.PNG",
+    deployedLink: "https://github.com/NickPrather11/liri-node-app",
+    githubLink: "https://github.com/NickPrather11/liri-node-app",
+    description: ""
+  },
+  {
+    name: "Eat Da Burger!",
+    image: "/client/public/images/burger_devourer.PNG",
+    deployedLink: "https://stark-eyrie-85216.herokuapp.com/",
+    githubLink: "",
+    description: ""
+  },
+  {
+    name: "Dream Journal",
+    image: "/client/public/images/dream_journal.PNG",
+    deployedLink: "https://cryptic-badlands-56041.herokuapp.com/",
+    githubLink: "https://github.com/NickPrather11/Project2",
+    description: ""
+  },
+  {
+    name: "The Hard Times Scraper",
+    image: "/client/public/images/hardTimesScraper.PNG",
+    deployedLink: "https://stark-tor-89874.herokuapp.com/",
+    githubLink: "",
+    description: ""
+  },
+  {
+    name: "Cloud Clicker Game",
+    image: "/client/public/images/cloud_clicker.PNG",
+    deployedLink: "https://evening-crag-14153.herokuapp.com/",
+    githubLink: "",
+    description: ""
+  },
+  {
+    name: "Google Books Search",
+    image: "/client/public/images/book_search.PNG",
+    deployedLink: "https://serene-beyond-02883.herokuapp.com/",
+    githubLink: "",
+    description: ""
+  },
+  {
+    name: "Texas Hold 'Em Poker",
+    image: "/client/public/images/poker.PNG",
+    deployedLink: "https://warm-garden-85112.herokuapp.com/",
+    githubLink: "",
+    description: ""
+  }
+];
