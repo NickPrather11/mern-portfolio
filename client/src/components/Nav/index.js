e5fb78997c80ec0d7cf855c6ce8d9675ce8cfa88
Flying Serpent <nickprather11@gmail.com>
2019-11-20 14:04:29 -0500

    added id's to nav links, first step in highlighting active tab

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,15 +4,15 @@ import "./style.css";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
-      <a className="navbar-brand" href="/about">
+      <a className="navbar-brand" id="about" href="/about">
         About Me
       </a>
       <h1 className="text-light">|</h1>
-      <a className="navbar-brand" href="/">
+      <a className="navbar-brand" id="portfolio" href="/">
         Portfolio
       </a>
       <h1 className="text-light">|</h1>
-      <a className="navbar-brand" href="/contact">
+      <a className="navbar-brand" id="contact" href="/contact">
         Contact
       </a>
     </nav>
