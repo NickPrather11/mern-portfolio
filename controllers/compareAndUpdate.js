4648296fc215980d6ee2b0f7e2cd29047a824f55
Flying Serpent <nickprather11@gmail.com>
2019-11-22 11:50:33 -0500

    fixed problem where db wouldn't populate if there was nothing already in there

--- controllers/compareAndUpdate.js
+++ controllers/compareAndUpdate.js
@@ -8,44 +8,56 @@ module.exports = {
       let dbNames = [];
       dbModel.forEach(item => dbNames.push(item.name));
       projectsArray.forEach(paItem => {
-        dbModel.forEach(dbItem => {
-          if (paItem.name === dbItem.name) {
-            let id = dbItem._id;
-            if (paItem.description !== dbItem.description) {
-              //update description
-              db.Project.updateOne({ _id: id }, { description: paItem.description }, { new: true }).catch(err =>
-                res.status(422).json(err)
-              );
+        if (dbNames.length > 0) {
+          dbModel.forEach(dbItem => {
+            if (paItem.name === dbItem.name) {
+              let id = dbItem._id;
+              if (paItem.description !== dbItem.description) {
+                //update description
+                db.Project.updateOne({ _id: id }, { description: paItem.description }, { new: true }).catch(err =>
+                  res.status(422).json(err)
+                );
+              }
+              if (paItem.image !== dbItem.image) {
+                //update image
+                db.Project.updateOne({ _id: id }, { image: paItem.image }, { new: true }).catch(err =>
+                  res.status(422).json(err)
+                );
+              }
+              if (paItem.deployedLink !== dbItem.deployedLink) {
+                //update deployedLink
+                db.Project.updateOne({ _id: id }, { deployedLink: paItem.deployedLink }, { new: true }).catch(err =>
+                  res.status(422).json(err)
+                );
+              }
+              if (paItem.githubLink !== dbItem.githubLink) {
+                //update githubLink
+                db.Project.updateOne({ _id: id }, { githubLink: paItem.githubLink }, { new: true }).catch(err =>
+                  res.status(422).json(err)
+                );
+              }
+            } else {
+              if (dbNames.indexOf(paItem.name) < 0) {
+                //creates new project object in the database
+                db.Project.create(paItem)
+                  .then(dbProject => {
+                    console.log("New project added: ", dbProject);
+                  })
+                  .catch(err => {
+                    console.log(err.message);
+                  });
+              }
             }
-            if (paItem.image !== dbItem.image) {
-              //update image
-              db.Project.updateOne({ _id: id }, { image: paItem.image }, { new: true }).catch(err =>
-                res.status(422).json(err)
-              );
-            }
-            if (paItem.deployedLink !== dbItem.deployedLink) {
-              //update deployedLink
-              db.Project.updateOne({ _id: id }, { deployedLink: paItem.deployedLink }, { new: true }).catch(err =>
-                res.status(422).json(err)
-              );
-            }
-            if (paItem.githubLink !== dbItem.githubLink) {
-              //update githubLink
-              db.Project.updateOne({ _id: id }, { githubLink: paItem.githubLink }, { new: true }).catch(err =>
-                res.status(422).json(err)
-              );
-            }
-          } else if (dbNames.indexOf(paItem.name) < 0) {
-            //creates new project object in the database
-            db.Project.create(paItem)
-              .then(dbProject => {
-                console.log("New project added: ", dbProject);
-              })
-              .catch(err => {
-                console.log(err.message);
-              });
-          }
-        });
+          });
+        } else {
+          db.Project.create(paItem)
+            .then(dbProject => {
+              console.log("New project added: ", dbProject);
+            })
+            .catch(err => {
+              console.log(err.message);
+            });
+        }
       });
     });
   }
